23050406d5f04f9d6803cc5feb100300c648289d
JoyLubega <jollankent@gmail.com>
2018-11-12 17:38:10 +0300

    Add port in Heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const itemController = require('./controllers/itemController.js');
 const app = express();
 app.use(bodyParser.json());
 
-app.listen(3000, ()=> console.log("Server started at port : 3000"))
+app.listen(process.env.PORT || 3000, ()=> console.log("Server started at port : 3000"))
 
 app.use('/api', shoppinglistController);
 app.use('/api/shoppinglist', itemController);
