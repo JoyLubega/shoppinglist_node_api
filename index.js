afa7c06fe017aa979d0e1d4e0806d2eb586b5740
JoyLubega <jollankent@gmail.com>
2018-11-18 18:41:47 +0300

    Edit cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const { mongoose } = require('./db.js');
 const shoppinglistController = require('./controllers/shoppinglistControllers.js')
@@ -8,14 +9,17 @@ const itemController = require('./controllers/itemController.js');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
+
 
 app.listen(process.env.PORT || 3000, ()=> console.log("Server started at port : 3000"))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
   
 app.use('/api', shoppinglistController);
 app.use('/api/shoppinglist', itemController);
