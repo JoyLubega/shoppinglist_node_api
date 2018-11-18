9bb289d30cd702b00c0f5bbcacaa1d148b1b4af5
JoyLubega <jollankent@gmail.com>
2018-11-18 18:59:49 +0300

    edit databaseUrl

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //mongodb://joyceN:wisdominGod2@ds159563 .mlab.com:59563/shoppinglists
-mongoose.connect('//mongodb://joyceN:wisdominGod2@ds159563.mlab.com:59563/shoppinglists',(err)=>{
+mongodb://joyceN:wisdominGod2@ds159563.mlab.com:59563/shoppinglists
+mongoose.connect('mongodb://joyceN:wisdominGod2@ds159563.mlab.com:59563/shoppinglists',(err)=>{
     if (!err)
         console.log('MongoDb connection succeedded');
     else
